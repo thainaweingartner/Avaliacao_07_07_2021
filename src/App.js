760adabd86e4c13d42306d0072f5baa3760dbea4
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2021-07-07 21:59:25 -0300

    App Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-no-target-blank */
+import { useEffect, useState } from "react";
+import { FaUserFriends } from "react-icons/fa";
+
+import { ChakraProvider } from "@chakra-ui/react"
+
+import "./App.css"
 
 function App() {
+  const [user, setUser] = useState({});
+  const [input, setInput] = useState();
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/exemple`)
+      .then(res => res.json())
+      .then(data => {
+        setUser(data);
+      });
+  }, [])
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    
+    fetch(`https://api.github.com/users/${input}`)
+    .then(res => res.json())
+    .then(data => {
+      setUser(data);
+      console.log(data);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <div className="navbar">Busca de Perfis do Github</div>
+        <form onSubmit={handleSearch} className="form">
+          <input className="input" placeholder="Nome do Usuário" onChange={e => setInput(e.target.value)}/>
+          <button className="button" type="submit">Pesquisar</button>
+        </form>
+        <div className="card">
+          <div className="card-img">
+            <img src={user.avatar_url} alt={user.name} />
+          </div>
+          <div className="card-content">
+            <h3>{user.name || 'Name'}</h3>
+            <h4>{user.login || 'User Name'}</h4>
+          </div>
+          <div className="card-footer">
+            <h4> 
+              <FaUserFriends /> 
+              {user.following} Following
+            </h4>
+            <h4> 
+              <FaUserFriends /> 
+              {user.followers} Followers
+            </h4>
+            <a href={user.public_repos} target="_blank"> 
+              Acessar Repositórios
+            </a>
+          </div>
+        </div>
+      </ChakraProvider>
     </div>
   );
 }
